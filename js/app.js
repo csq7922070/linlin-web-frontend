50a264fdac4c4ea82becf61915c8c4c740bfe508
chengshuiqiang <chengshuiqiang@4zlink.com>
2016-01-12 12:14:05 +0800

    解决代码合并冲突。

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 var basePath = "http://mifan.4zlink.com:8080/mifan";
 //var basePath = "http://localhost:8080/skh";
-<<<<<<< HEAD
-
-=======
 //var basePath = "http://192.168.0.117:8080/skh";
->>>>>>> develop
+
 var myApp = angular.module('myApp', ['ui.router', 'angular-carousel', 'skhControllers']);
 
 myApp.config(function ($stateProvider, $urlRouterProvider) {
