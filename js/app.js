f4b85f5adb39121ddcbd2ab24f080e5a5097d0a1
chengshuiqiang <chengshuiqiang@4zlink.com>
2016-01-12 12:08:15 +0800

    添加了npm包配置文件package.json

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-//var basePath = "http://mifan.4zlink.com:8080/mifan";
-var basePath = "http://localhost:8080/skh";
+var basePath = "http://mifan.4zlink.com:8080/mifan";
+//var basePath = "http://localhost:8080/skh";
 
 var myApp = angular.module('myApp', ['ui.router', 'angular-carousel', 'skhControllers']);
 
