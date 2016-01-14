470023033e995fbe16df12fd2c55f7523a44cc5a
chengshuiqiang <chengshuiqiang@4zlink.com>
2016-01-14 12:35:57 +0800

    对css合js做了合并处理，目前第三方库的js和css没做合并，项目自定义的js合并为all.js，项目自定义的css合并为all.css。

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var del = require('del');
 var sourcemaps = require('gulp-sourcemaps');
+var rimraf = require('rimraf');
 
 ///////////
 // Build //
@@ -23,12 +24,15 @@ gulp.task('copy', function() {
   .pipe(gulp.dest('dist/tpl'));
   gulp.src('app/images/**')
   .pipe(gulp.dest('dist/images'));
-  gulp.src('bower_components/**')
+  gulp.src('bower_components/**/*min.js')
+  .pipe(gulp.dest('dist/bower_components'));
+  gulp.src('bower_components/**/*min.css')
   .pipe(gulp.dest('dist/bower_components'));
 });
 
 gulp.task('minifycss', function(){
-  return gulp.src('app/css/*.css')      //压缩的文件
+  return gulp.src('app/css/*.css')      
+        .pipe(concat('all.css'))//压缩的文件
         .pipe(rename({suffix: '.min'}))   //输出文件夹
         .pipe(minifycss())
         .pipe(gulp.dest('dist/css'));   //执行压缩
@@ -58,20 +62,25 @@ gulp.task('concat:filters', function () {
         .pipe(gulp.dest('dist/js/temp'));
 });
 
-gulp.task('concat:js', ['concat:controllers', 'concat:directives', 'concat:services', 'concat:filters']);
+//gulp.task('concat:js', ['concat:controllers', 'concat:directives', 'concat:services', 'concat:filters']);
 
 gulp.task('minifyjs', function(){
-  return gulp.src(['app/js/app.js','dist/js/temp/*.js'])      //压缩的文件
+  return gulp.src(['app/js/app.js','app/js/controllers/*','app/js/directives/*','app/js/services/*','app/js/filters/*'])
+        .pipe(concat('all.js'))      //压缩的文件
         .pipe(rename({suffix: '.min'}))   //输出文件重命名
         .pipe(uglify()) //执行压缩
         .pipe(gulp.dest('dist/js'));   
 });
 
-gulp.task('build:js', ['concat:js'], function(){
+gulp.task('clean:dist', function (cb) {
+  rimraf('dist', cb);
+});
+
+gulp.task('build:js', [], function(){
   runSequence(['minifyjs']);
 });
 
-gulp.task('build', [], function () {
+gulp.task('build', ['clean:dist'], function () {
   runSequence(['copy', 'build:js', 'minifycss']);
 });
 
