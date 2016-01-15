008c92438ab1961acb6d76f0791faeb744b9cad7
chengshuiqiang <chengshuiqiang@4zlink.com>
2016-01-15 16:14:38 +0800

    添加了e2e功能测试。

--- test/e2e/spec/scenarios.js
+++ test/e2e/spec/scenarios.js
@@ -7,35 +7,91 @@ describe('my app', function() {
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/home/shop-info/1");
   });
 
 
-  describe('view1', function() {
+  describe('#/home/shop-info/1', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('#/home/shop-info/1');
     });
 
 
     it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+      expect(element.all(by.css('.cell_phone')).first().getText()).
+        toMatch('电话');
     });
 
   });
 
 
-  describe('view2', function() {
+  describe('#/home/shop-info/2', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view2');
+      browser.get('#/home/shop-info/2');
     });
 
 
     it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+      expect(element.all(by.css('.cell_phone')).first().getText()).
+        toMatch('电话');
+    });
+
+  });
+
+  describe('#/home/shop-info/3', function() {
+
+    beforeEach(function() {
+      browser.get('#/home/shop-info/3');
+    });
+
+
+    it('should render view3 when user navigates to /view3', function() {
+      expect(element.all(by.css('.online_err')).first().getText()).
+        toMatch('程序员哥哥');
+    });
+
+  });
+
+  describe('#/repair-list', function() {
+
+    beforeEach(function() {
+      browser.get('#/repair-list');
+    });
+
+
+    it('should render view3 when user navigates to repair-list', function() {
+      expect(element.all(by.css('.check')).first().getText()).
+        toMatch('查看');
+    });
+
+  });
+
+  describe('#/repair/44', function() {
+
+    beforeEach(function() {
+      browser.get('#/repair/44');
+    });
+
+
+    it('should render view3 when user navigates to #/repair/44', function() {
+      expect(element.all(by.css('.date_time')).first().getText()).
+        toMatch('2016-01-14');
+    });
+
+  });
+
+  describe('#/repair-add', function() {
+
+    beforeEach(function() {
+      browser.get('#/repair-add');
+    });
+
+
+    it('should render view3 when user navigates to #/repair-add', function() {
+      expect(element.all(by.css('[name]=mobile')).first().getText()).
+        toMatch('2016-01-14');
     });
 
   });
