1bc6314cb4440e57c5e5f55f2a8a81c0de50e2e1
baoqingxin <1142634487@qq.com>
2016-01-20 11:29:48 +0800

    添加更改默认地址功能，修复报修，投诉意见部分bug

--- app/address/address-list.js
+++ app/address/address-list.js
@@ -9,48 +9,48 @@ angular.module('app.address').controller('addressListCtrl', ['$stateParams', '$s
             console.log("获取业主信息成功");
             if (data != "") {
                 vm.houses = data.items;
-                //    if (house.active == 0) {
-                //        vm.activeId = house.id;
-                //    }
-                    vm.deleteAddress = function (id) {
-                        //console.log("click")
-                        vm.sure_delete = true;
-                        vm.sure = function () {
-                            vm.sure_delete = false;
-                            params = {
-                                id: id
-                            }
-                            addresses.delete(params).$promise.then(function (data) {
-                                //vm.houses.splice(a, 1);
-                                console.log("删除成功")
-                            }, function (data) {
-                                console.log("删除失败")
-                            })
-                        }
-                        vm.cancel = function () {
-                            vm.sure_delete = false;
-                        }
-                    };
-                    vm.change_flag = function (id) {
-                        //if (house.active == 0) {
-                        //    return;
-                        //}
-                        console.log("改变功能")
-                        params = {
-                            id: id,
-                            openid: sessionStorage.getItem("openid")
-                        }
-                        addresses.save(params).$promise.then(function () {
-                            vm.activeId = id;
-                            console.log("设置默认地址成功");
-                        }, function (data) {
-                            console.log("设置默认地址失败");
-                        })
-                    }
+                   vm.houses.forEach(function(h){
+                       if(h.active==0){
+                           vm.activeId= h.id;
+                       }
+                   })
             }
-
         },function(data){
             console.log("获取业主信息失败")
         })
+
+        vm.deleteAddress = function (house) {
+            vm.sure_delete = true;
+            vm.sure = function () {
+                vm.sure_delete = false;
+                params = {
+                    id: house.id
+                }
+                addresses.delete(params).$promise.then(function (data) {
+                    console.log("删除成功")
+                    house.rowState=1;
+                }, function (data) {
+                    console.log("删除失败")
+                })
+            }
+        };
+
+        vm.cancel = function () {
+            vm.sure_delete = false;
+        }
+
+        vm.change_flag = function (house) {
+            if (house.active == 0) {
+                return;
+            }
+            params = {
+                id: house.id,
+                openid: sessionStorage.getItem("openid")
+            }
+            addresses.save(params).$promise.then(function () {
+                vm.activeId = house.id;
+            }, function (data) {
+            })
+        }
     }
 ])
