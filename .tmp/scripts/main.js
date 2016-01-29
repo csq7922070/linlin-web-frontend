2ea5a133a4a94cb1255f6f3801da8a3d220109b8
chengshuiqiang <chengshuiqiang@4zlink.com>
2016-01-29 12:43:59 +0800

    水电支付页面控制器代码做了重构。

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -721,71 +721,11 @@ angular.module('app.payment').controller('paymentCtrl', ['$scope', '$http', '$st
         }
         var tmpwmonth = $rootScope.wmonth.map(_parseInt).sort(compare);
         var tmpemonth = $rootScope.emonth.map(_parseInt).sort(compare);
-        var wdate;
-        var edate;
-        $scope.watmonth = arrange(tmpwmonth);
-        $scope.elmonth = arrange(tmpemonth);
+        var wmonthSections = arrange(tmpwmonth);
+        var emonthSections = arrange(tmpemonth);
 
-        if ($scope.watmonth.length > 1) {
-            $scope.wyear = (tmpwmonth[0] + "").substr(0, 4);
-            if ($scope.watmonth[0].length > 1) {
-                if ((tmpwmonth[1] + "").substr(0, 4) == $scope.wyear) {
-                    $scope.wmonth = (tmpwmonth[0] + "").substr(4, 2) + "," + (tmpwmonth[1] + "").substr(4, 2) + "月等";
-                    wdate = $scope.wyear + "年" + $scope.wmonth;
-                } else {
-                    wdate = (tmpwmonth[0] + "").substr(0, 4) + "年" + (tmpwmonth[0] + "").substr(4, 2) + "月," + (tmpwmonth[0] + "").substr(0, 4) + "年" + (tmpwmonth[0] + "").substr(4, 2) + "月等";
-                }
-            } else {
-                $scope.wmonth = (tmpwmonth[0] + "").substr(4, 2) + "月";
-                wdate = $scope.wyear + "年" + $scope.wmonth;
-            }
-
-        } else {
-            $scope.wyear = (tmpwmonth[0] + "").substr(0, 4) + "年";
-            $scope.wmonth = (tmpwmonth[0] + "").substr(4, 2);
-            if(tmpwmonth[0] != tmpwmonth[tmpwmonth.length - 1]){
-                 $scope.wmonth += "-" + (tmpwmonth[tmpwmonth.length - 1] + "").substr(4, 2)
-            } 
-            $scope.wmonth += "月";
-            wdate = $scope.wyear + $scope.wmonth;
-        }
-
-        if ($scope.elmonth.length > 1) {
-            $scope.eyear = (tmpemonth[0] + "").substr(0, 4);
-            if ($scope.elmonth[0].length > 1) {
-                if ((tmpemonth[1] + "").substr(0, 4) == $scope.eyear) {
-                    $scope.emonth = (tmpemonth[0] + "").substr(4, 2) + "," + (tmpemonth[1] + "").substr(4, 2) + "月等";
-                    edate = $scope.eyear + "年" + $scope.emonth;
-                } else {
-                    edate = (tmpemonth[0] + "").substr(0, 4) + "年" + (tmpemonth[0] + "").substr(4, 2) + "月," + (tmpemonth[0] + "").substr(0, 4) + "年" + (tmpemonth[0] + "").substr(4, 2) + "月等";
-                }
-            } else {
-                $scope.emonth = (tmpemonth[0] + "").substr(4, 2) + "月";
-                edate = $scope.eyear + "年" + $scope.emonth;
-            }
-
-        } else {
-            $scope.eyear = (tmpemonth[0] + "").substr(0, 4) + "年";
-            $scope.emonth = (tmpemonth[0] + "").substr(4, 2);
-            if(tmpemonth[0] != tmpemonth[tmpemonth.length - 1]){
-                 $scope.emonth += "-" + (tmpemonth[tmpemonth.length - 1] + "").substr(4, 2)
-            } 
-            $scope.emonth += "月";
-            edate = $scope.eyear + $scope.emonth;
-        }
-
-        $scope.watmonth = wdate;
-        $scope.elmonth = edate;
-
-        $scope.block = $stateParams.block;
-        $scope.unit = $stateParams.unit;
-        $scope.room = $stateParams.room;
-
-        $scope.waterFr = $rootScope.waterFree;
-        $scope.eleFr = $rootScope.eleFree;
-
-        $scope.totalFee = $rootScope.waterFree + $rootScope.eleFree;
-
+        $scope.watmonth = getMergeDate(wmonthSections, tmpwmonth);
+        $scope.elmonth = getMergeDate(emonthSections, tmpemonth);
 
         function arrange(array) {
             var t;
@@ -814,6 +754,97 @@ angular.module('app.payment').controller('paymentCtrl', ['$scope', '$http', '$st
         function _parseInt() {
             return parseInt(arguments[0]);
         }
+
+        function getMergeDate(monthSections, tmpmonth){
+            var year,month;
+            var date;
+            if (monthSections.length > 1) {
+                year = (tmpmonth[0] + "").substr(0, 4);
+                if (monthSections[0].length > 1) {
+                    if ((tmpmonth[1] + "").substr(0, 4) == year) {
+                        month = (tmpmonth[0] + "").substr(4, 2) + "," + (tmpmonth[1] + "").substr(4, 2) + "月等";
+                        date = year + "年" + month;
+                    } else {
+                        date = (tmpmonth[0] + "").substr(0, 4) + "年" + (tmpmonth[0] + "").substr(4, 2) + "月," + (tmpmonth[0] + "").substr(0, 4) + "年" + (tmpmonth[0] + "").substr(4, 2) + "月等";
+                    }
+                } else {
+                    month = (tmpmonth[0] + "").substr(4, 2) + "月";
+                    date = year + "年" + month;
+                }
+
+            } else {
+                year = (tmpmonth[0] + "").substr(0, 4) + "年";
+                month = (tmpmonth[0] + "").substr(4, 2);
+                if(tmpmonth[0] != tmpmonth[tmpmonth.length - 1]){
+                     month += "-" + (tmpmonth[tmpmonth.length - 1] + "").substr(4, 2)
+                } 
+                month += "月";
+                date = year + month;
+            }
+            return date;
+        }
+        // var wyear,wmonth;
+        // if ($scope.watmonth.length > 1) {
+        //     wyear = (tmpwmonth[0] + "").substr(0, 4);
+        //     if ($scope.watmonth[0].length > 1) {
+        //         if ((tmpwmonth[1] + "").substr(0, 4) == wyear) {
+        //             wmonth = (tmpwmonth[0] + "").substr(4, 2) + "," + (tmpwmonth[1] + "").substr(4, 2) + "月等";
+        //             wdate = wyear + "年" + wmonth;
+        //         } else {
+        //             wdate = (tmpwmonth[0] + "").substr(0, 4) + "年" + (tmpwmonth[0] + "").substr(4, 2) + "月," + (tmpwmonth[0] + "").substr(0, 4) + "年" + (tmpwmonth[0] + "").substr(4, 2) + "月等";
+        //         }
+        //     } else {
+        //         wmonth = (tmpwmonth[0] + "").substr(4, 2) + "月";
+        //         wdate = wyear + "年" + wmonth;
+        //     }
+
+        // } else {
+        //     wyear = (tmpwmonth[0] + "").substr(0, 4) + "年";
+        //     wmonth = (tmpwmonth[0] + "").substr(4, 2);
+        //     if(tmpwmonth[0] != tmpwmonth[tmpwmonth.length - 1]){
+        //          wmonth += "-" + (tmpwmonth[tmpwmonth.length - 1] + "").substr(4, 2)
+        //     } 
+        //     wmonth += "月";
+        //     wdate = wyear + wmonth;
+        // }
+
+        // var eyear,emonth;
+        // if ($scope.elmonth.length > 1) {
+        //     eyear = (tmpemonth[0] + "").substr(0, 4);
+        //     if ($scope.elmonth[0].length > 1) {
+        //         if ((tmpemonth[1] + "").substr(0, 4) == eyear) {
+        //             emonth = (tmpemonth[0] + "").substr(4, 2) + "," + (tmpemonth[1] + "").substr(4, 2) + "月等";
+        //             edate = eyear + "年" + emonth;
+        //         } else {
+        //             edate = (tmpemonth[0] + "").substr(0, 4) + "年" + (tmpemonth[0] + "").substr(4, 2) + "月," + (tmpemonth[0] + "").substr(0, 4) + "年" + (tmpemonth[0] + "").substr(4, 2) + "月等";
+        //         }
+        //     } else {
+        //         emonth = (tmpemonth[0] + "").substr(4, 2) + "月";
+        //         edate = eyear + "年" + emonth;
+        //     }
+
+        // } else {
+        //     eyear = (tmpemonth[0] + "").substr(0, 4) + "年";
+        //     emonth = (tmpemonth[0] + "").substr(4, 2);
+        //     if(tmpemonth[0] != tmpemonth[tmpemonth.length - 1]){
+        //          emonth += "-" + (tmpemonth[tmpemonth.length - 1] + "").substr(4, 2)
+        //     } 
+        //     emonth += "月";
+        //     edate = eyear + emonth;
+        // }
+
+        // $scope.watmonth = wdate;
+        // $scope.elmonth = edate;
+
+        $scope.block = $stateParams.block;
+        $scope.unit = $stateParams.unit;
+        $scope.room = $stateParams.room;
+
+        $scope.waterFr = $rootScope.waterFree;
+        $scope.eleFr = $rootScope.eleFree;
+
+        $scope.totalFee = $rootScope.waterFree + $rootScope.eleFree;
+
 
         $scope.money_payment = function() {
             console.log("支付功能开始");
@@ -875,37 +906,6 @@ angular.module('app.payment').controller('paymentCtrl', ['$scope', '$http', '$st
 
     }
 ]);
-
-(function() {
-    angular.module('app.shop').controller('shopInfoCtrl', ['$scope',  '$stateParams', '$rootScope', 'shops',
-        function($scope, $stateParams, $rootScope, shops) {
-            $rootScope.site = $stateParams.site;
-            $scope.currentPage = 0;
-            $scope.pageSize = 5;
-            $scope.shops = [];
-
-            $scope.load = function(goPage, limit) {
-                if (goPage > $scope.numberOfPages || $scope.currentPage == goPage || $scope.busy) {
-                    return;
-                } else if ($rootScope.site != 3) {
-                    $scope.busy = true;
-                    params = {
-                        offset: $scope.pageSize * (goPage - 1),
-                        limit: limit == 8 ? limit : $scope.pageSize,
-                        type: $stateParams.site - 1
-                    }
-                    shops.query(params).$promise.then(function(data) {
-                        $scope.numberOfPages = Math.ceil(data.count / $scope.pageSize);
-                        $scope.currentPage = goPage;
-                        $scope.busy = false;
-                        $scope.shops.push.apply($scope.shops, data.items);
-                    });
-                }
-            }
-            $scope.load(1, 8);
-        }
-    ]);
-})();
 
 (function() {
     angular.module('app.repair').controller('repairAddCtrl', ['$timeout', '$state', 'repairs',
@@ -1051,6 +1051,37 @@ angular.module('app.repair').controller('repairListCtrl', ['$timeout', '$state',
         vm.load(1, vm.pageSize);
     }
 ]);
+
+(function() {
+    angular.module('app.shop').controller('shopInfoCtrl', ['$scope',  '$stateParams', '$rootScope', 'shops',
+        function($scope, $stateParams, $rootScope, shops) {
+            $rootScope.site = $stateParams.site;
+            $scope.currentPage = 0;
+            $scope.pageSize = 5;
+            $scope.shops = [];
+
+            $scope.load = function(goPage, limit) {
+                if (goPage > $scope.numberOfPages || $scope.currentPage == goPage || $scope.busy) {
+                    return;
+                } else if ($rootScope.site != 3) {
+                    $scope.busy = true;
+                    params = {
+                        offset: $scope.pageSize * (goPage - 1),
+                        limit: limit == 8 ? limit : $scope.pageSize,
+                        type: $stateParams.site - 1
+                    }
+                    shops.query(params).$promise.then(function(data) {
+                        $scope.numberOfPages = Math.ceil(data.count / $scope.pageSize);
+                        $scope.currentPage = goPage;
+                        $scope.busy = false;
+                        $scope.shops.push.apply($scope.shops, data.items);
+                    });
+                }
+            }
+            $scope.load(1, 8);
+        }
+    ]);
+})();
 
 myApp.directive('errSrc', function() {
   return {
