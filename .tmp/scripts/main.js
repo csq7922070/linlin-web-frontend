4ed59d1c7bd23a260ca9d99b85b5d64a218fb6df
baoqingxin <1142634487@qq.com>
2016-02-02 17:33:47 +0800

    改变弹出框确定和取消的位置

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -273,6 +273,52 @@ angular.module('app.address').controller('addressCtrl', ['$stateParams', 'addres
     }
 ]);
 
+angular.module('app.home').controller('homeCtrl', ['$scope', '$http', '$stateParams', '$rootScope', '$state', '$location',
+    function($scope, $http, $stateParams, $rootScope, $state, $location) {
+
+        var url = $location.url().substring($location.url().indexOf("?"));
+        if (url.indexOf("home") != -1) {
+            url = "";
+        }
+        //1.6获取微信用户openid
+        if (sessionStorage.getItem("openid") == null) {
+            $http({
+                method: "GET",
+                url: basePath + '/getopenid' + url
+            }).success(function(data) {
+                sessionStorage.setItem("openid", data.openid);
+                //添加微信支付
+                sessionStorage.setItem("timestamp", data.timestamp);
+                sessionStorage.setItem("noncestr", data.noncestr);
+                sessionStorage.setItem("sign", data.sign);
+                console.log("获取openid成功");
+            }).error(function(data) {
+                console.log("获取openid失败");
+            });
+        }
+
+        $scope.slides7 = [{
+            id: 10,
+            label: "slide #1",
+            img: "images/banner_01.png"
+        }, {
+            id: 11,
+            label: "slide #2",
+            img: "images/banner_02.png"
+        }, {
+            id: 12,
+            label: "slide #3",
+            img: "images/banner_03.png"
+        }];
+        $scope.carouselIndex7 = 0;
+
+        $rootScope.site = 1;
+        $state.go("home.shop-info", {
+            site: 1
+        });
+    }
+]);
+
 angular.module('app.complain').controller('complainAddCtrl', ['$timeout', '$state', 'complains',
     function ($timeout, $state, complains) {
         var vm = this;
@@ -351,52 +397,6 @@ angular.module('app.complain').controller('complainListCtrl', ['complains',
         vm.load(1, vm.pageSize);
     }
 ]);
-angular.module('app.home').controller('homeCtrl', ['$scope', '$http', '$stateParams', '$rootScope', '$state', '$location',
-    function($scope, $http, $stateParams, $rootScope, $state, $location) {
-
-        var url = $location.url().substring($location.url().indexOf("?"));
-        if (url.indexOf("home") != -1) {
-            url = "";
-        }
-        //1.6获取微信用户openid
-        if (sessionStorage.getItem("openid") == null) {
-            $http({
-                method: "GET",
-                url: basePath + '/getopenid' + url
-            }).success(function(data) {
-                sessionStorage.setItem("openid", data.openid);
-                //添加微信支付
-                sessionStorage.setItem("timestamp", data.timestamp);
-                sessionStorage.setItem("noncestr", data.noncestr);
-                sessionStorage.setItem("sign", data.sign);
-                console.log("获取openid成功");
-            }).error(function(data) {
-                console.log("获取openid失败");
-            });
-        }
-
-        $scope.slides7 = [{
-            id: 10,
-            label: "slide #1",
-            img: "images/banner_01.png"
-        }, {
-            id: 11,
-            label: "slide #2",
-            img: "images/banner_02.png"
-        }, {
-            id: 12,
-            label: "slide #3",
-            img: "images/banner_03.png"
-        }];
-        $scope.carouselIndex7 = 0;
-
-        $rootScope.site = 1;
-        $state.go("home.shop-info", {
-            site: 1
-        });
-    }
-]);
-
 (function() {
     angular.module('app.notice').controller("noticeDetailCtrl", ['$stateParams', 'notices',
         function($stateParams, notices) {
