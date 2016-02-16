f2b52eaec998cdba7214178be0985d1f1ab424a1
chengshuiqiang <chengshuiqiang@4zlink.com>
2016-02-16 19:58:32 +0800

    在app-home页面中添加了原始的home页和新的我们页，页面切换用了选择效果。

--- app/home/app-home.js
+++ app/home/app-home.js
@@ -1,5 +1,14 @@
-angular.module('app.home').controller('appHomeCtrl', ['$stateParams',
-    function ($stateParams) {
-        var vm = this;
+angular.module('app.home').controller('appHomeCtrl', ['$stateParams','$scope',
+    function ($stateParams, $scope) {
+    	var currentNav = 'home';
+        $scope.nav = function(name){
+        	if(name=='account' && currentNav == 'home'){
+        		document.querySelector("#flip-container").classList.toggle("flip");
+        		currentNav = 'account';
+        	}else if(name == 'home' && currentNav == 'account'){
+        		document.querySelector("#flip-container").classList.toggle("flip");
+        		currentNav = 'home';
+        	}
+        }
     }
 ]);
