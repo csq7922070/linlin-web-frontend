cf0739e8004230bde822570d0da962482a2e7f43
李佳惠 <2909875@163.com>
2016-02-18 19:36:49 +0800

    初步修改样式

--- app/repair/repair-add.js
+++ app/repair/repair-add.js
@@ -1,6 +1,6 @@
 (function() {
-    angular.module('app.repair').controller('repairAddCtrl', ['$timeout', '$state', 'repairs',
-        function($timeout, $state, repairs) {
+    angular.module('app.repair').controller('repairAddCtrl', ['$timeout', '$state', 'repairs','address','$scope',
+        function($timeout, $state, repairs, address, $scope) {
             var vm = this;
 
             vm.mask_close = function() {
@@ -10,6 +10,7 @@
                 vm.err_show = false;
             }
             vm.submitForm = function() {
+                vm.repair.device = $scope.currentDevice.name;
                 vm.repair.openid=sessionStorage.getItem("openid");
                 params = vm.repair;
                 repairs.save(params).$promise.then(successcb, errcb);
@@ -29,6 +30,18 @@
                     vm.err_show = false;
                 }, 3000);
             }
+
+            $scope.decives=[{name:'开/换锁'},{name:'供电照明'},{name:'抽水马桶'},{name:'上/下水管道'},{name:'门窗维修'},{name:'房屋主体'},{name:'电梯/门禁'},{name:'供暖设施'},{name:'其他'}];
+            $scope.decives.push();
+            $scope.currentDevice = $scope.decives[0];
+
+            console.log(address.getDefaultAddress());
+            $scope.defaultcommunity = address.getDefaultAddress().community;
+            $scope.defaultblock = address.getDefaultAddress().block;
+            $scope.defaultunit = address.getDefaultAddress().unit;
+            $scope.defaultroom = address.getDefaultAddress().room;
+            
+
         }
     ]);
 })();
