97cdadd121feafdb2bb9df6718cd29c9f8b6f584
chengshuiqiang <chengshuiqiang@4zlink.com>
2016-02-24 13:50:01 +0800

    将所有Js文件名重构为-隔开，单词首字母都小写，添加了用户协议和免责声明页面。

---
+++ app/account/disclaimer/disclaimer.js
@@ -0,0 +1,6 @@
+angular.module('app.account').controller('disclaimerCtrl', ['$stateParams', '$scope','account','errorLog',
+    'userInfo','address','control','$state',
+    function ($stateParams,$scope,account,errorLog,userInfo,address,control,$state) {
+        
+    }
+]);
