fb0b6afe7196976287d579b4311728b9d3671943
csq7922070 <275091651@qq.com>
2016-03-06 20:23:23 +0800

    完成了缴费记录页面数据绑定

--- app/pay/pay-list.js
+++ app/pay/pay-list.js
@@ -1,9 +1,12 @@
 angular.module('app.pay').controller('payListCtrl', ['$scope', '$http', '$stateParams', '$rootScope', '$state', '$filter', 
     'addressInfo','$q','userInfo','errorLog','billPay',
     function ($scope, $http, $stateParams, $rootScope, $state, $filter,addressInfo,$q,userInfo,errorLog,billPay) {
+    	$scope.showLoading = true;
         billPay.getPayList().then(function(data){
+        	$scope.showLoading = false;
             $scope.payList = data;
         },function(reason){
+        	$scope.showLoading = false;
             alert(errorLog.getErrorMessage(reason));
         });
     }
